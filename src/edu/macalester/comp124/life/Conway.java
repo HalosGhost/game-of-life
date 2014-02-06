06415f51f7ead52258f1c786cf489250aca9e8ce
Sam Stuewe <halosghost@archlinux.info>
2014-02-06 10:16:30 -0600

    Complete the ruleset

--- src/edu/macalester/comp124/life/Conway.java
+++ src/edu/macalester/comp124/life/Conway.java
@@ -19,7 +19,9 @@ public class Conway implements RuleSet {
      * @return true if the cell should be alive in the next generation.
      */
     public boolean applyRules(boolean isAlive, int neighborCount) {
-        return true;
+        if (neighborCount == 2) return isAlive; // Cell is unchanged
+        else if (neighborCount == 3) return true; // Cell is born
+        else return false; // Cell is too lonely or is overcrowded
     }
 }
 
